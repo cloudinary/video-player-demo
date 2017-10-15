d2e558c7c65904ccb9562302b49d920fd4c3d22d
hadarbj <hadarbj@gmail.com>
2017-10-15 17:09:27 +0300

    Update video.js

--- docs/js/video.js
+++ docs/js/video.js
@@ -128,6 +128,35 @@ eventTypes.forEach(function(eventType) {
                               transformation: {streaming_profile: 'hd' } });
   
  var plistplayer = cld.videoPlayer('demo-playlist-player');
+
+ function playMe() {
+  var val = parseInt(this.val());
+  plistplayer.playlist().playAtIndex(val);
+}
+
+function updateOnSrc() {
+  var plist = plistplayer.playlist();
+  var playing = plist.currentIndex();
+  var loop = plist.length();
+  for(var i=0; i<loop; i++) {
+    var label = "list"+i;
+    var btn = document.getElementById(label);
+    if(i == playing)
+      btn.setAttribute("class", "thumbnail playing");
+    else
+      btn.setAttribute("class", "thumbnail ");
+  }
+  console.log("updateOnSrc", playing, loop);
+}
+
+var plistplayer = cld.videoPlayer('demo-playlist-player');
+plistplayer.on('sourcechanged', updateOnSrc);
+plistplayer.playlist(
+  [{ publicId: 'game2', sourceTypes: ['dash'], transformation: {streaming_profile: 'hd' }},
+   { publicId: 'hls_30s_test', sourceTypes: ['dash'], transformation: {streaming_profile: 'hd' }},
+   { publicId: 'hd_trim2', sourceTypes: ['dash'], transformation: {streaming_profile: 'hd' }},
+   { publicId: 'Homepage_2', sourceTypes: ['dash'], transformation: {streaming_profile: 'hd' }}], 
+   { autoAdvance: 0, repeat: true });
   
  plistplayer.source('game2',{ sourceTypes: ['hls'], 
                               transformation: {streaming_profile: 'hd' } });
