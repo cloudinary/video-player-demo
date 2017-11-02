f1214ae6e22fc3e3fd7a7bb1039a182068a0fed9
hadarbj <hadarbj@gmail.com>
2017-11-03 01:26:34 +0200

    Update draft.js

--- docs/js/draft.js
+++ docs/js/draft.js
@@ -27,10 +27,9 @@ function requestAuto(auto) {
 function changeOfResolution() {
   var index = qualityLevels.selectedIndex;
   var res = document.getElementById(qualityLevels[index].width);
-  console.log("changeOfResolution() ",qualityLevels[index].width);
   var old = document.getElementsByClassName("range-info second-hover");
   for (var i = 0; i < old.length; i++) {
-    oldactive[i].setAttribute("class","range-info");
+    old[i].setAttribute("class","range-info");
   }
   res.setAttribute("class","range-info second-hover");
 }
