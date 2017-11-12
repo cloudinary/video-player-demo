a7c7a292f963168053260b2799c9daf3216deac4
hadarbj <hadarbj@gmail.com>
2017-11-12 17:20:35 +0200

    Update draft.js

--- docs/js/draft.js
+++ docs/js/draft.js
@@ -5,10 +5,15 @@ function toggleClass() {
 function setTheme(btn) {
     var newTheme = "cld-video-player-skin-" + btn.value;
     var oldTheme = "cld-video-player-skin-";
-    if(btn.value == "dark")
+    var pagination = document.getElementById("video-pagination");
+    if(btn.value == "dark") {
         oldTheme += "light"; 
-    else
+        pagination.classList.remove("style01");
+    }
+    else {
         oldTheme += "dark";
+        pagination.classList.add("style01");
+    }
     var vplayers = document.getElementsByClassName("cld-video-player");
     for(var i = 0; i < vplayers.length; i++) {
         vplayers[i].classList.remove(oldTheme);
