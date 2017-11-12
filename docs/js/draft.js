2e2eed2a299780cfd211d164e17964a844a5dffc
hadarbj <hadarbj@gmail.com>
2017-11-12 16:57:34 +0200

    Update draft.js

--- docs/js/draft.js
+++ docs/js/draft.js
@@ -99,7 +99,7 @@ if ((chrome) && (safari)) safari = false;
 if(safari)
   document.getElementById("checkbox").disabled = true;
 
-playerSizeChange(document.getElementById("myRange"));
+playerSizeChange(document.getElementById("player-range"));
 
 var cld = cloudinary.Cloudinary.new({ cloud_name: 'hadar' });
 
