9682f6f303d633a3e1c71255fd974ff3f0cb6297
hadarbj <hadarbj@gmail.com>
2017-11-28 10:25:14 -0800

    Update transcode.js

--- docs/js/transcode.js
+++ docs/js/transcode.js
@@ -112,7 +112,7 @@ function updateProgress() {
     if(transcriptProgress < 100)
         updateTranscriptProgress()
     if (autoTagProgress < 100 || transcriptProgress < 100)
-        setTimeout(updateProgress,2000);
+        setTimeout(updateProgress,1500);
 }
 
 function getData() {
